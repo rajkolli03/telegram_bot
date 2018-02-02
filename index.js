c4073524b72f496293bd9aa6128489d02e6449d3
rajkolli03 <rajendraatiitg@gmail.com>
2018-02-02 12:10:16 +0530

    Modifying to add express dependency and webhook implementation
    Signed-off-by: rajkolli03 <rajendraatiitg@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const Telegraf = require('telegraf')
+const express = require('express');
+const expressApp = express();
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL;
+
+const bot = new Telegraf(BOT_TOKEN);
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+expressApp.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
+
+/*
+ your bot commands and all the other stuff on here ....
+*/
 
 bot.start((ctx) => {
   console.log('started:', ctx.from.id)
@@ -12,6 +24,11 @@ bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
 bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 
-bot.startPolling();
 
-
+// and at the end just start server on PORT
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+expressApp.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
